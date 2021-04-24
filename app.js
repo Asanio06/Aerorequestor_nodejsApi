b67b050ee694e1e55f90746aeb89fd7c054c21b1
Lansana <lansana.diomande@epfedu.fr>
2021-04-25 01:39:26 +0200

    Configuration de eslint

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+
 const app = express();
 
 const port = 8000;
 
 app.listen(port, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Listening on port ${port}`);
+  // eslint-disable-next-line no-console
+  console.log(`Listening on port ${port}`);
 });
