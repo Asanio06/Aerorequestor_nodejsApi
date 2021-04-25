39d2957b9c46429fc0df76e6dc13cbdb9305ae1e
Lansana <lansana.diomande@epfedu.fr>
2021-04-25 11:02:35 +0200

    Get weather data by airport

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const weatherRouter = require('./routes/weather.route');
 
 const app = express();
 
 const port = 8000;
+
+app.use('/api/weather', weatherRouter);
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: process.env.BDD_HOST,
-    database: process.env.BDD_NAME,
-    user: process.env.BDD_USER,
-    password: process.env.BDD_PASSWORD,
-}
+  host: process.env.BDD_HOST,
+  database: process.env.BDD_NAME,
+  user: process.env.BDD_USER,
+  password: process.env.BDD_PASSWORD,
+};
